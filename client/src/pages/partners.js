1b2e32655e82327c24cfea0d452fe91565dd197a
vasilvlaykov <vasil.a.vlaykov@gmail.com>
2021-04-11 10:30:12 +0200

    bugfix

--- client/src/pages/partners.js
+++ client/src/pages/partners.js
@@ -29,8 +29,8 @@ export default function Partners() {
         <>
             <Card style={{ border: "none", minWidth: "800px", display: "flex", justifyContent: "center", alignSelf: "center" }}>
                 <h2 style={{ textAlign: "center" }}>Партньори</h2>
-                {partnersList ? partnersList.map((partner) =>
-                    <Card style={{ maxHeight: "60px", alignSelf: "center", margin:"0.5rem" }}>
+                {partnersList ? partnersList.map((partner, index) =>
+                    <Card key={index} style={{ maxHeight: "60px", alignSelf: "center", margin:"0.5rem" }}>
                         <a href={partner.url} target="blank"><img src={partner.img} alt="partner-img" style={{height: "60px"}}/></a>
                         <Button type="button" onClick={() => app.database().ref(`partners/${partner.id}`).remove()} style={{ display: currentUser && currentUser.email === 'abv@abv.bgr' ? "flex" : "none", position: "absolute", top: "0", right: "0", padding: "0 0.6rem", background: "#FF9100", borderColor: "#FF9100", fontWeight: "bold" }}>x</Button>
                     </Card>
